2e89eed8a77af8a368b916b56ffbb0ad69f4fb55
gui <guie32014@gmail.com>
2025-12-04 13:49:00 -0300

    Modificando arquivo insert_records.py para testar conexão

--- insert_records.py
+++ insert_records.py
@@ -1,8 +1,21 @@
+import psycopg2
 from api_request import mock_fetch_data
 
 def connect_to_db():
     print("Conectando ao database do PostgreSQL...")
     try:
-        
-    except:
+        conn = psycopg2.connect(
+            host="localhost",
+            port=5000,
+            dbname="db"
+            user="db_user",
+            password="db_passsword"
+        )
+
+        print(conn)
+    except: psycopg2.Error as e:
+        print(f"Conexão falhou {e}")
+        raise
+
+connect_to_db()
 
