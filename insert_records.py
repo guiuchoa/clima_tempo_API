5b0dab025c1b3581a8e731a7e463a91042fb3c5e
gui <guie32014@gmail.com>
2025-12-05 11:23:05 -0300

    Adicionando função de inserir dados no banco

--- insert_records.py
+++ insert_records.py
@@ -13,13 +13,12 @@ def connect_to_db():
         )
 
         return conn
-
-        print(conn)
+    
     except psycopg2.Error as e:
         print(f"Conexão falhou {e}")
         raise
 
-connect_to_db()
+#connect_to_db()
 
 def create_table(conn):
     print("Criando tabela caso não exista...")
@@ -27,19 +26,68 @@ def create_table(conn):
         cursor = conn.cursor()
         cursor.execute('''
             CREATE SCHEMA IF NOT EXISTS dev;
-                       CREATE TABLE IF NOT EXISTS dev.dados_brutos (
-                        id SERIAL PRIMARY KEY,
-                        cidade TEXT,
-                        temperatura FLOAT,
-                        descricao_tempo TEXT,
-                        velocidade_vento FLOAT,
-                        tempo TIMESTAMP,
-                        inserido_em TIMESTAMP DEFAULT NOW(),
-                        utc_offset TEXT
-                       )
+            CREATE TABLE IF NOT EXISTS dev.dados_brutos (
+             id SERIAL PRIMARY KEY,
+             cidade TEXT,
+             temperatura FLOAT,
+             descricao_tempo TEXT,
+             velocidade_vento FLOAT,
+             tempo TIMESTAMP,
+             inserido_em TIMESTAMP DEFAULT NOW(),
+             utc_offset TEXT
+            )
         ''')
         conn.commit()
         print("Tabela criada.")
     except psycopg2.Error as e:
         print(f"Criação da tabela falhou: {e}")
+        raise
     
+
+def insert_records(conn, data):
+    print("Inserindo dados no banco")
+
+    try:
+        tempo_clima = data['current']
+        local = data['location']
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO dev.dados_brutos (
+                       cidade,
+                       temperatura,
+                       descricao_tempo,
+                       velocidade_vento,
+                       tempo,
+                       inserido_em,
+                       utc_offset
+                    ) VALUES (%s, %s, %s, %s, %s, NOW(), %s)
+        ''', (
+                local['name'],
+                tempo_clima['temperature'],
+                tempo_clima['weather_descriptions'][0],
+                tempo_clima['wind_speed'],
+                local['localtime'],
+                local['utc_offset']
+        ))
+        conn.commit()
+        print("Dados inseridos com sucesso.")
+
+    except psycopg2.Error as e:
+        print(f"Erro ao inserir dados: {e}")
+        raise
+
+
+def main():
+    try:
+        data = mock_fetch_data()
+        conn = connect_to_db()
+        create_table(conn)
+        insert_records(conn, data)
+    except psycopg2.Error as e:
+        print(f"Ocorreu um erro no processo: {e}")
+    finally:
+        if 'conn' in locals():
+            conn.close()
+            print("Conexão fechada")
+
+main()
